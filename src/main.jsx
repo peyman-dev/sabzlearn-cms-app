64f111a56ff3cfeb2df85ce1a19e03be2df75e72
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-08 23:06:24 +0330

    Fonts.css added to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
-import routes from "./core/routes.js";
+import routes from "./core/routes.jsx";
+
+import "./../public/css/index.css";
+import "./../public/css/fonts.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
