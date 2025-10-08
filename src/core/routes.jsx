44fda702a337452f020e3a3bf9c43d2e701d1ccd
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-08 23:05:04 +0330

    routes.js changed to .jsx

---
+++ src/core/routes.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/home/index.jsx";
+import UsersPage from "../pages/users/page.jsx";
+import CommentsPage from "../pages/comments/page.jsx";
+import ProductsPage from "../pages/products/page.jsx";
+import TicketsPage from "../pages/tickets/page.jsx";
+import ApplicationLayout from "../components/templates/app-layout/index.jsx";
+
+const routes = createBrowserRouter([
+  {
+    element: <ApplicationLayout />,
+    children: [
+      {
+        index: true, // index: true == HomePage ["/"]
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/users",
+        element: <UsersPage />,
+      },
+      {
+        path: "/comments",
+        element: <CommentsPage />,
+      },
+      {
+        path: "/tickets",
+        element: <TicketsPage />,
+      },
+    ],
+  },
+]);
+
+export default routes;
