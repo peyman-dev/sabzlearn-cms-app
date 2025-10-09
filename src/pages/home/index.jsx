742504c0abc6b198ea6a2a4688c829359482fc77
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-09 08:57:47 +0330

    Added summaries & Header to /home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/templates/home/header/index.jsx'
+import Summaries from '../../components/templates/home/summaries/index.jsx'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Header />
+      <Summaries />
+    </>
   )
 }
 
